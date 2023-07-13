a739c233b7032050ec651cca885a06c4ba03dad0
fartenadze <nfartenadze89@gmail.com>
2023-07-13 17:48:58 +0400

    style: aligned icon properly

--- src/pages/login-page/index.tsx
+++ src/pages/login-page/index.tsx
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 import LoginSharpIcon from "@mui/icons-material/LoginSharp";
 
 import LoginForm from "../../components/form";
+import { Grid } from "@mui/material";
 
 const LoginPage = () => {
   const gradient =
@@ -35,12 +36,13 @@ const LoginPage = () => {
       </Container>
       <Container sx={formContainerStyle}>
         <Box>
-          <Box>
+          <Box display={"flex"} justifyContent={"center"}>
             <LoginSharpIcon
               sx={{
-                fontSize: "50px",
-                color: "rgba(108, 184, 254, 1)",
+                fontSize: "8rem",
+                color: "rgba(108, 90, 254, 1)",
                 mb: "40px",
+                mr: "30px",
               }}
             />
           </Box>
